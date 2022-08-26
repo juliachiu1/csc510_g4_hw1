9a6422e7d5db28300d3a49a0f696bb98dfc8a598
Hsueh-YANG <prettyjim841231@gmail.com>
2022-08-26 11:42:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Testing functions for Homework 1',
     author='Ching-Lun Chiu, Hsueh-Yang Yu, Po-Hsun Lin, Li-Ling Ku, Chen-Hsuan Chiang',
     author_email='hyu25@ncsu.edu',
-    url='https://github.com/juliachiu1/csc510_g4_hw1/blob/main/README.md',
+    url='https://github.com/juliachiu1/csc510_g4_hw1',
     packages=['Django','pandas','matplotlib'],
     long_description= '''\
     	Hi, this is homework 1 code. Here we are testing the function.
