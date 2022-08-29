7ca11d0009ac623ed236852e4fed7d38b319dde1
juliachiu1 <112150278+juliachiu1@users.noreply.github.com>
2022-08-29 14:15:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Ching-Lun Chiu, Hsueh-Yang Yu, Po-Hsun Lin, Li-Ling Ku, Chen-Hsuan Chiang',
     author_email='hyu25@ncsu.edu',
     url='https://github.com/juliachiu1/csc510_g4_hw1/blob/main/README.md',
-    packages=['homework_group4'],
+    packages=[''],
     long_description= '''\
     	Hi, this is homework 1 code. Here we are testing the function.
     	'''
