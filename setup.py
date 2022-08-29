7aa9ec245c45114a05540d94f3cff1de0fea9e94
juliachiu1 <112150278+juliachiu1@users.noreply.github.com>
2022-08-29 14:13:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 
 setup(
-    name='csc510_g4_hw1',
+    name='homework_group4',
     version='1.0',
     description='Testing functions for Homework 1',
     author='Ching-Lun Chiu, Hsueh-Yang Yu, Po-Hsun Lin, Li-Ling Ku, Chen-Hsuan Chiang',
     author_email='hyu25@ncsu.edu',
     url='https://github.com/juliachiu1/csc510_g4_hw1/blob/main/README.md',
-    packages=['homework_group4'],
+    packages=[''],
     long_description= '''\
     	Hi, this is homework 1 code. Here we are testing the function.
     	'''
